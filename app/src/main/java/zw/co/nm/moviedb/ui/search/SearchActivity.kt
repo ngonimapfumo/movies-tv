543c08f429fa367c25cb4cafd30c54b58b62ffca
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2023-06-01 15:57:10 +0200

    add base files search

--- app/src/main/java/zw/co/nm/moviedb/ui/search/SearchActivity.kt
+++ app/src/main/java/zw/co/nm/moviedb/ui/search/SearchActivity.kt
@@ -1,16 +1,18 @@
 package zw.co.nm.moviedb.ui.search
 
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.launch
 import zw.co.nm.moviedb.databinding.ActivitySearchBinding
-import zw.co.nm.moviedb.ui.adapters.MovieListAdapter
 import zw.co.nm.moviedb.ui.adapters.SearchAdapter
 import zw.co.nm.moviedb.ui.movie.MovieViewModel
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     lateinit var binding: ActivitySearchBinding
     lateinit var movieViewModel: MovieViewModel
     private lateinit var adapter: SearchAdapter
@@ -19,14 +21,27 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.searchView.setOnQueryTextListener(this)
         movieViewModel = ViewModelProvider(this)[MovieViewModel::class.java]
-        lifecycleScope.launch {
-            movieViewModel.searchMovie("aliens").collect { response ->
-                val data = response.data!!.body()!!.results
-                adapter = SearchAdapter(data)
-                binding.searchRecycler.adapter = adapter
-            }
 
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                movieViewModel.searchMovie(query!!).collect { response ->
+                    val data = response.data!!.body()!!.results
+                    adapter = SearchAdapter(data)
+                    binding.searchRecycler.adapter = adapter
+                }
+
+            }
         }
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        // TODO("Not yet implemented")
+        return false
     }
 }
