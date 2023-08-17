69add7b167f77ed5c6022a1a00db89ff9350130f
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2023-08-17 18:32:26 +0200

    dev_build add collection

--- app/src/main/java/zw/co/nm/moviedb/ui/collection/CollectionActivity.kt
+++ app/src/main/java/zw/co/nm/moviedb/ui/collection/CollectionActivity.kt
@@ -3,10 +3,14 @@ package zw.co.nm.moviedb.ui.collection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import zw.co.nm.moviedb.R
+import zw.co.nm.moviedb.databinding.ActivityCollectionBinding
+import zw.co.nm.moviedb.databinding.ItemMovieMainBinding
 
 class CollectionActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCollectionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_collection)
+        binding = ActivityCollectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
