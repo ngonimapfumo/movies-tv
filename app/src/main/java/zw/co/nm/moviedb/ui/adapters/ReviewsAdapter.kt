5ae29961743c686432b94eeaef3a93887572114b
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2023-09-07 21:23:28 +0200

    add tv trailers

--- app/src/main/java/zw/co/nm/moviedb/ui/adapters/ReviewsAdapter.kt
+++ app/src/main/java/zw/co/nm/moviedb/ui/adapters/ReviewsAdapter.kt
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import zw.co.nm.moviedb.data.remote.networkmodel.GetReviewsResponse
 import zw.co.nm.moviedb.databinding.ItemReviewDetailBinding
+import java.time.LocalDate
+import java.time.OffsetDateTime
+import java.time.format.TextStyle
+import java.util.Locale
 
 class ReviewsAdapter(private var data: List<GetReviewsResponse.Result>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -20,13 +24,23 @@ class ReviewsAdapter(private var data: List<GetReviewsResponse.Result>) :
     override fun getItemCount(): Int = data.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-       binding!!.textViewAuthorName.text = data[position].author
-       binding!!.textViewContent.text = data[position].content
-        binding!!.reviewedDate.text = data[position].createdAt
-        holder.itemView.setOnClickListener { AlertDialog.Builder(holder.itemView.context)
-            .setMessage(data[position].content)
-            .setPositiveButton("Dismiss",null)
-            .show()}
+        val localDate = OffsetDateTime.parse(data[position].createdAt)
+        binding!!.textViewAuthorName.text = data[position].author
+        binding!!.textViewContent.text = data[position].content
+        binding!!.reviewedDate.text = buildString {
+            append("--")
+            append(localDate.dayOfMonth)
+            append(" ")
+            append(localDate.month.getDisplayName(TextStyle.SHORT, Locale.ENGLISH))
+            append(" ")
+            append(localDate.year)
+        }
+        holder.itemView.setOnClickListener {
+            AlertDialog.Builder(holder.itemView.context)
+                .setMessage(data[position].content)
+                .setPositiveButton("Dismiss", null)
+                .show()
+        }
 
     }
 
