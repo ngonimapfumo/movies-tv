50c17aa3ae77d28700ac640daf3cbe2e06375e76
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2023-10-04 10:00:46 +0200

    clean up

--- app/src/main/java/zw/co/nm/moviedb/presentation/tv/episode/EpisodeAdapter.kt
+++ app/src/main/java/zw/co/nm/moviedb/presentation/tv/episode/EpisodeAdapter.kt
@@ -25,7 +25,10 @@ class EpisodeAdapter(private var data: List<zw.co.nm.moviedb.data.remote.model.r
         val imgPath = data[position].stillPath
         binding!!.textViewEpisodeName.text = data[position].name
         binding!!.textViewShortSummary.text = data[position].overview
-        binding!!.textViewEpisodeNumber.text = "Episode ${data[position].episodeNumber}"
+        binding!!.textViewEpisodeNumber.text = buildString {
+            append("Episode ")
+            append(data[position].episodeNumber)
+        }
         Picasso.get().load(Constants.IMAGE_BASE_URL + imgPath)
             .placeholder(R.drawable.sample_episode_exp)
             .into(binding!!.imageView)
