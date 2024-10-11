edb52f23bd13d65b30b662e1795f0c8f9606c11e
NgonidzasheMapfumo <ngonidzdashe.mapfumo@ecocashholdings.co.zw>
2024-10-11 11:06:43 +0200

    add compose components

---
+++ app/src/main/java/zw/co/nm/moviedb/composables/TopBar.kt
@@ -0,0 +1,26 @@
+package zw.co.nm.moviedb.composables
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SmallTopAppBar(
+    text: String
+) {
+
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text(text)
+        }
+    )
+}
+
